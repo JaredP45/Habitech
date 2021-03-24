b96237a4684583799d73c7bad57e79dd059efab9
Jared <paubelj45@gmail.com>
2021-03-24 09:37:44 -0500

    Initial Commit

---
+++ SwifToDo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwifToDo
+//
+//  Created by student on 3/24/21.
+//  Copyright © 2021 IN185 BS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
